5cd98457625c3d98c5f8212ab354597585e09688
VictoriaBel788 <valerovictoria299@gmail.com>
2025-09-17 17:10:13 -0500

    inicio de validacion con expresion

---
+++ script.js
@@ -0,0 +1,7 @@
+let camposForm = document.querySelectorAll(".formulario__entrada")
+
+camposForm.forEach(function(campoForm){
+campoForm.addEventListener("keyup", function(){
+    console.log("Presionando una tecla... ");
+})
+})
