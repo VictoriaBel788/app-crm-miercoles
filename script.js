2865f7158bd33283053fe01b8144082ccabc2a95
jaimezzapata <zapataval2304@gmail.com>
2025-10-08 16:45:58 -0500

    Introducción a localStorage

--- script.js
+++ script.js
@@ -6,9 +6,54 @@ camposForm.forEach((campoForm) => {
         switch (e.target.id) {
             case "nombre":
                 if (expresionesRegulares.regexTexto.test(e.target.value)) {
-                    console.log("Cumple la expresión regular")
+                    document.getElementById("nombre").style.outline = "none"
+                    document.getElementById("nombre").style.borderColor = "green"
+                    document.getElementById("nombre").style.borderWidth = "2px"
+                    document.getElementById("nombre").style.color = "black"
                 } else {
-                    console.log("No cumple la expresión regular")
+                    document.getElementById("nombre").style.outline = "none"
+                    document.getElementById("nombre").style.borderColor = "red"
+                    document.getElementById("nombre").style.borderWidth = "2px"
+                    document.getElementById("nombre").style.color = "red"
+                }
+                break;
+            case "apellido":
+                if (expresionesRegulares.regexTexto.test(e.target.value)) {
+                    document.getElementById("apellido").style.outline = "none"
+                    document.getElementById("apellido").style.borderColor = "green"
+                    document.getElementById("apellido").style.borderWidth = "2px"
+                    document.getElementById("apellido").style.color = "black"
+                } else {
+                    document.getElementById("apellido").style.outline = "none"
+                    document.getElementById("apellido").style.borderColor = "red"
+                    document.getElementById("apellido").style.borderWidth = "2px"
+                    document.getElementById("apellido").style.color = "red"
+                }
+                break;
+            case "documento":
+                if (expresionesRegulares.regexNumero.test(e.target.value)) {
+                    document.getElementById("documento").style.outline = "none"
+                    document.getElementById("documento").style.borderColor = "green"
+                    document.getElementById("documento").style.borderWidth = "2px"
+                    document.getElementById("documento").style.color = "black"
+                } else {
+                    document.getElementById("documento").style.outline = "none"
+                    document.getElementById("documento").style.borderColor = "red"
+                    document.getElementById("documento").style.borderWidth = "2px"
+                    document.getElementById("documento").style.color = "red"
+                }
+                break;
+            case "telefono":
+                if (expresionesRegulares.regexNumero.test(e.target.value)) {
+                    document.getElementById("telefono").style.outline = "none"
+                    document.getElementById("telefono").style.borderColor = "green"
+                    document.getElementById("telefono").style.borderWidth = "2px"
+                    document.getElementById("telefono").style.color = "black"
+                } else {
+                    document.getElementById("telefono").style.outline = "none"
+                    document.getElementById("telefono").style.borderColor = "red"
+                    document.getElementById("telefono").style.borderWidth = "2px"
+                    document.getElementById("telefono").style.color = "red"
                 }
                 break;
         }
